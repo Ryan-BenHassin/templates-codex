18fcb301fae64e210ae98f62b0d806fc57364208
ameniR <raddaouiameni59@gmail.com>
2024-05-10 10:23:45 +0100

    validate Scheduling template

--- Scheduling/Scheduling_service_1395.py
+++ Scheduling/Scheduling_service_1395.py
@@ -1,4 +1,3 @@
-# TODO
 import schedule
 import time
 import datetime
@@ -6,15 +5,18 @@ import datetime
 def job(message):
     print(f"Job executed at {datetime.datetime.now()} - {message}")
 
-def schedule_jobs():
+def setup_schedules():
     schedule.every(10).minutes.do(job, message='10 minutes job')
     schedule.every().hour.do(job, message='hourly job')
     schedule.every().day.at("10:30").do(job, message='daily job')
     schedule.every(5).to(6).minutes.do(job, message='5 to 6 minutes job')
+    schedule.every(1).minutes.do(job, message='1 minute job')
 
+def run_schedules():
+    setup_schedules()
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+      schedule.run_pending()
+      time.sleep(1)
 
 if __name__ == "__main__":
-    schedule_jobs()
+    run_schedules() 
